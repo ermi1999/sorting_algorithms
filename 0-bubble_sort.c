85eeb237eaae195c9414238e97ad3082cf5b7dbf
ermi1999 <ermiyasabiye1999@gmail.com>
2023-11-16 13:18:30 -0800

    added a print_array to bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,28 +1,28 @@
 #include "sort.h"
-#include <stdio.h>
-
 /**
- * bubble_sort - sorts an array in ascending order
- * @array: array of ints to sort
- * @size: size of the array
+ * bubble_sort - sorts the array using the bubble sort algorithm.
+ * @array: The array to be sorted.
+ * @size: The size of the array.
+ * Return: Nothing.
  */
 void bubble_sort(int *array, size_t size)
 {
-	unsigned int i, j;
-	int tmp;
+	size_t i;
+	size_t j;
+	int temp;
 
-	if (size < 2)
+	if (array == NULL || size < 2)
 		return;
-
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
-				tmp = array[j];
+				temp = array[j];
 				array[j] = array[j + 1];
-				array[j + 1] = tmp;
+				array[j + 1] = temp;
+				print_array(array, size);
 			}
 		}
 	}
