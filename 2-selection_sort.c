c771bc2881fa523fc9b72e64c36c6d4858bae708
ermi1999 <ermiyasabiye1999@gmail.com>
2023-11-17 11:46:41 -0800

    fixed betty and added prototype for selection

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,27 +7,28 @@
  */
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, idx;
-    int temp;
+	size_t i, j, idx;
+	int temp;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    for (i = 0; i < size; i++)
-    {
+	for (i = 0; i < size; i++)
+	{
 
-        idx = i;
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[idx])
-                idx = j;
-        }
-        if (idx != i)
-        {
-            temp = array[i];
-            array[i] = array[idx];
-            array[idx] = temp;
-            print_array(array, size);
-        }
-    }
+		idx = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[idx])
+				idx = j;
+		}
+		if (idx != i)
+		{
+			temp = array[i];
+			array[i] = array[idx];
+			array[idx] = temp;
+			print_array(array, size);
+		}
+	}
 }
+
