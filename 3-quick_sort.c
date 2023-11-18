63b0729ba9931c10668eb3a49813a999a6254fc4
ermi1999 <ermiyasabiye1999@gmail.com>
2023-11-18 12:34:10 -0800

    fixed betty error for quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,1 +1,73 @@
+#include "sort.h"
+
+/**
+ * partition - partitions an array for quick sort.
+ * @array: The array to be partitioned.
+ * @low: The starting index of the partition.
+ * @high: The ending index of the partition.
+ * @size: The size of the array.
+ * Return: The index of the pivot element.
+ */
+int partition(int *array, int low, int high, int size)
+{
+	int pivot, i, j, temp;
+
+	pivot = array[high];
+	i = low - 1;
+	for (j = low; j <= high - 1; j++)
+	{
+		if (array[j] < pivot)
+		{
+			i++;
+			if (i != j)
+			{
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+	i++;
+	if (i != high)
+	{
+		temp = array[i];
+		array[i] = array[high];
+		array[high] = temp;
+		print_array(array, size);
+	}
+	return (i);
+}
+
+/**
+ * quicksort_recursive - recursive function to perform quicksort.
+ * @array: The array to be sorted.
+ * @low: The starting index of the array.
+ * @high: The ending index of the array.
+ * @size: The size of the array.
+ */
+void quicksort_recursive(int *array, int low, int high, int size)
+{
+	int pivot;
+
+	if (low < high)
+	{
+		pivot = partition(array, low, high, size);
+		quicksort_recursive(array, low, pivot - 1, size);
+		quicksort_recursive(array, pivot + 1, high, size);
+	}
+}
+
+/**
+ * quick_sort - sorts an array using quick sort algorithm.
+ * @array: The array to be sorted.
+ * @size: The size of the array.
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+
+	quicksort_recursive(array, 0, size - 1, size);
+}
 
