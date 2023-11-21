d315c6c90af80e3038c32642c1297caf0e82b10c
ermi1999 <ermiyasabiye1999@gmail.com>
2023-11-21 14:23:22 -0800

    counting sort

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,6 +1,3 @@
-counting sort
-
-```
 #include "sort.h"
 #include <stdlib.h>
 /**
@@ -12,40 +9,39 @@ counting sort
 
 void counting_sort(int *array, size_t size)
 {
-    int *output, *count;
-    size_t i, max;
+	int *output, *count;
+	size_t i, max;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    max = 0;
+	max = 0;
 
-    for (i = 0; i < size; i++)
-    {
-        if (array[i] > (int)max)
-            max = array[i];
-    }
-    count = malloc((max + 1) * sizeof(int));
-    output = malloc(size * sizeof(int));
-    for (i = 0; i < size; i++)
-        count[array[i]]++;
-    for (i = 1; i <= max; i++)
-        count[i] += count[i - 1];
-    print_array(count, max + 1);
-    for (i = 0; i < size; i++)
-        output[i] = array[i];
-    for (i = size - 1; i > 0; i--)
-    {
-        output[count[array[i]] - 1] = array[i];
-        count[array[i]] -= 1;
-    }
-    output[count[array[0]] - 1] = array[0];
-    count[array[0]] -= 1;
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] > (int)max)
+			max = array[i];
+	}
+	count = malloc((max + 1) * sizeof(int));
+	output = malloc(size * sizeof(int));
+	for (i = 0; i < size; i++)
+		count[array[i]]++;
+	for (i = 1; i <= max; i++)
+		count[i] += count[i - 1];
+	print_array(count, max + 1);
+	for (i = 0; i < size; i++)
+		output[i] = array[i];
+	for (i = size - 1; i > 0; i--)
+	{
+		output[count[array[i]] - 1] = array[i];
+		count[array[i]] -= 1;
+	}
+	output[count[array[0]] - 1] = array[0];
+	count[array[0]] -= 1;
 
-    for (i = 0; i < size; i++)
-        array[i] = output[i];
+	for (i = 0; i < size; i++)
+		array[i] = output[i];
 
-    free(count);
-    free(output);
+	free(count);
+	free(output);
 }
-```
